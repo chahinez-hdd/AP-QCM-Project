562eb7983dbe66641109a3d0e42913fe1be12eee
chahinez-hdd <chahinez.haddad2005@gmail.com>
2025-01-18 15:11:25 +0100

    Add user login and registration functionality and finished the user part

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ class QCMApp:
         self.categories = []
         self.initialize_files()
 
-
+    """ici je gère la connexion de l'utilisateur"""
+    username = input("Entrez votre nom d'utilisateur : ").strip()
+    
+    if username in self.users_data:
+        print(f"\nBienvenue de retour, {username}!")
+        self.display_user_history(username)
+    else:
+        print(f"\nNouveau utilisateur créé : {username}")
+        self.users_data[username] = {"history": []}
+        self.save_users_data()
+    
+    self.current_user = username
 
     def display_user_history(self, username: str):
         """j'affiche ici l'historique QCM d'un utilisateur"""
@@ -60,4 +71,3 @@ class QCMApp:
         with open("data/questions.json", "r", encoding="utf-8") as f:
             self.questions = json.load(f)
             self.categories = list(self.questions.keys())
-
